b7346243ee2dc61b28a6efce59069a9a17633417
Ryota Ikezawa <pavegy@gmail.com>
2019-05-23 03:50:01 +0900

    Menus

--- lib/munchsrb.rb
+++ lib/munchsrb.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 require 'munchsrb/version'
+require 'munchsrb/aggregator'
 
 module Munchsrb
-  class Error < StandardError; end
+  class Error < StandardError;
+  end
   # Your code goes here...
+  ag = Munchsrb::Aggreagtor.new
+  ag.open_page
+  ag.print_menus
 end
